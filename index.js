3cbb0305ce976c367f1e1055dafc41c3fdcbbe68
shawnmcclure <shawn850mcclure@gmail.com>
2023-10-02 18:45:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers,names){
+    return drivers.filter((parameter) => parameter.toUpperCase().includes(names.toUpperCase()))
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toUpperCase().substring(0, letters.length) === letters.toUpperCase());
+}
+
+function matchName(drivers, argument){ 
+    return drivers.filter(driver => driver.name === argument)
+}
